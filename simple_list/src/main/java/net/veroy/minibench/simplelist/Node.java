4d615b9c645be17f0a25fe3fa2e32e510decf1b2
Raoul Veroy <rveroy@cs.tufts.edu>
2016-07-26 15:27:46 -0400

    Initial commit. Some copy-pasta files from etanalyzer-cpp that need to be cleaned up later.

---
+++ simple_list/src/main/java/net/veroy/minibench/simplelist/Node.java
@@ -0,0 +1,16 @@
+package net.veroy.minibench.simplelist;
+
+/**
+ * Node for SimpleList
+ *
+ */
+
+public class Node
+{
+    public int[] buffer;
+
+    public Node( int newNumber )
+    {
+        buffer = new int[newNumber];
+    }
+}
---
+++ simple_list/src/main/java/net/veroy/minibench/simplelist/SimpleList.java
@@ -0,0 +1,56 @@
+package net.veroy.minibench.simplelist;
+
+/**
+ * Simple LinkedList example
+ *
+ */
+import java.util.LinkedList;
+import net.veroy.minibench.simplelist.Node;
+
+public class SimpleList
+{
+    public static void main( String[] args )
+    {
+        int number = 0;
+        int reps = 0;
+        int node_size = 0;
+        // First argument is number of elements in LinkedList
+        try {
+            number = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println( "Argument " + args[0] + " not an integer." );
+            System.exit(1);
+        }
+        // Second argument is how many times to create the linked list
+        try {
+            reps = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println( "Argument " + args[1] + " not an integer." );
+            System.exit(1);
+        }
+        // Third argument is how big the Node array is.
+        try {
+            node_size = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println( "Argument " + args[2] + " not an integer." );
+            System.exit(1);
+        }
+        LinkedList mylist = new LinkedList();
+        for (int iter = 0; iter < reps; iter++) {
+            for (int i = 0; i < number; i++) {
+                mylist.add(new Node(node_size));
+                if ((i % 10) == 1) {
+                    mylist.remove();
+                }
+            }
+            System.out.println("Run[ " + iter + " ] -->  length: " + mylist.size());
+            while (!mylist.isEmpty()) {
+                mylist.remove();
+            }
+        }
+    }
+
+    private static void print_help()
+    {
+    }
+}
