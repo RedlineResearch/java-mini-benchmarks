9e5bd83fb58a45834224380c5e7710d53ef259ae
Raoul Veroy <raoul@littletechtips.com>
2016-08-04 02:04:45 -0400

    Adding SimpleListInt to use Integer instead of ints.

---
+++ simplelistInt/src/main/java/net/veroy/minibench/simplelistint/Node.java
@@ -0,0 +1,16 @@
+package net.veroy.minibench.simplelistint;
+
+/**
+ * Node for SimpleListInt
+ *
+ */
+
+public class Node
+{
+    public Integer value;
+
+    public Node( Integer newNumber )
+    {
+        value = newNumber;
+    }
+}
