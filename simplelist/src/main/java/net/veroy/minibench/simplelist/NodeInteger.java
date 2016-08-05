44af0c7f032244ad3e5bea95736609726d2c44ac
Raoul Veroy <raoul@littletechtips.com>
2016-08-04 23:07:29 -0400

    Add the NodeInteger class. NOTE: I'm not using generics here so that the classes are simple. Removing all possible complications.

---
+++ simplelist/src/main/java/net/veroy/minibench/simplelist/NodeInteger.java
@@ -0,0 +1,17 @@
+package net.veroy.minibench.simplelist;
+
+/**
+ * NodeInteger for SimpleList 
+ * (using class Integer instead of primitive int)
+ *
+ */
+
+public class NodeInteger
+{
+    public Integer value;
+
+    public NodeInteger( Integer newNumber )
+    {
+        value = newNumber;
+    }
+}
